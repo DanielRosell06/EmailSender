f5d5b391b56023e61a67e7c95ce8e7bec1f6d51e
DanielRosell06 <danielanicetorosell@gmail.com>
2025-09-03 16:07:46 -0300

    Criando encriptação de senha de usuario

--- Backend/crud/usuario_smtp.py
+++ Backend/crud/usuario_smtp.py
@@ -1,16 +1,38 @@
-#crud/usuario_smtp
-
+from cryptography.fernet import Fernet
 from sqlalchemy.orm import Session
-import models # Acessa a pasta acima para importar models.py
+import os
+from dotenv import load_dotenv
+
+import models
 from schema import usuario_smtp as schema_usuario_smtp
 
+# 1. Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+key_string = os.getenv("SMTP_KEY")
+
+f = Fernet(key_string.encode())
+
+def encrypt_password(password: str) -> str:
+    encrypted_password = f.encrypt(password.encode())
+    return encrypted_password.decode()
+
+
+def decrypt_password(encrypted_password: str) -> str:
+    decrypted_password = f.decrypt(encrypted_password.encode())
+
+    return decrypted_password.decode()
+
+
 def create_user_smtp(db: Session, new_usuario_smtp: schema_usuario_smtp.UsuarioSmtp):
+    encrypted_password = encrypt_password(new_usuario_smtp.Senha)
     usuario = models.UsuarioSmtp(
-        Usuario = new_usuario_smtp.Usuario,
-        Senha = new_usuario_smtp.Senha,
-        Dominio = new_usuario_smtp.Dominio,
-        Porta = new_usuario_smtp.Porta,
-    )    
+        Usuario=new_usuario_smtp.Usuario,
+        Senha=encrypted_password, # Salva a senha encriptada
+        Dominio=new_usuario_smtp.Dominio,
+        Porta=new_usuario_smtp.Porta,
+    )
+    
     db.add(usuario)
     db.commit()
     db.refresh(usuario)
