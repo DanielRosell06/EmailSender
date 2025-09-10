be701580e1870103853d0e2b0861577870e354e5
DanielRosell06 <danielanicetorosell@gmail.com>
2025-09-10 13:57:44 +0000

    Abrindo para rota https (arrumando bugs)

--- Backend/main.py
+++ Backend/main.py
@@ -1,13 +1,15 @@
 # main.py
 import os
 import importlib
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from database import engine, Base, get_db
 import socketio
 import uvicorn
 import asyncio
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 # Cria as tabelas no banco de dados
 Base.metadata.create_all(bind=engine)
@@ -29,13 +31,6 @@ app.add_middleware(
 # Cria a aplicação ASGI combinada para ser o ponto de entrada principal
 socket_app = socketio.ASGIApp(sio, other_asgi_app=app)
 
-
-# Define a rota raiz do FastAPI
-@app.get("/")
-def read_root():
-    return {"message": "Bem-vindo à API!"}
-
-
 # Lida com eventos de conexão e desconexão do SocketIO
 @sio.on('connect')
 async def connect(sid, environ):
@@ -45,20 +40,21 @@ async def connect(sid, environ):
 async def disconnect(sid):
     print(f'Desconectado: {sid}')
 
-
 # Importa os roteadores dinamicamente
 ROUTERS_DIR = os.path.join(os.path.dirname(__file__), "rotas")
 
+# O código de inclusão deve estar dentro do loop para cada roteador
 for filename in os.listdir(ROUTERS_DIR):
     if filename.endswith(".py") and filename not in ["__init__.py", "__pycache__"]:
         module_name = filename[:-3]
         module_path = f"rotas.{module_name}"
         router_module = importlib.import_module(module_path)
-        
         if hasattr(router_module, "router"):
             router_instance = getattr(router_module, "router")
-            app.include_router(router_instance)
+            app.include_router(router_instance, prefix="/api")
 
+# Define o caminho para a pasta 'dist'
+frontend_dist_path = Path(__file__).parent.parent / "FrontEnd" / "dist"
 
-# Você precisará executar a aplicação usando uvicorn na linha de comando
-# uvicorn main:socket_app --reload
+# Diz que servirá um conteúdo estático
+app.mount("/", StaticFiles(directory=frontend_dist_path, html=True), name="static")
